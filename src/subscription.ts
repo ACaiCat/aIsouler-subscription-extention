3f56ce110d8ffdf689a17801a29e239d78f3882e
Cai <13110818005@qq.com>
2026-01-15 13:01:18 +0800

    fix: 修复小程序重启后无法匹配

--- src/subscription.ts
+++ src/subscription.ts
@@ -6,7 +6,7 @@ import globalGroups from './globalGroups';
 export default defineGkdSubscription({
   id: 114516,
   name: 'Cai的AIsouler规则扩展',
-  version: 1,
+  version: 2,
   author: 'Cai',
   checkUpdateUrl: 'https://raw.githubusercontent.com/ACaiCat/aIsouler-subscription-extention/refs/heads/main/dist/gkd.version.json5',
   supportUri: 'https://github.com/ACaiCat/aIsouler-subscription-extention',
