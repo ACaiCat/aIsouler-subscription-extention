23015c833d92492e34b9592aff9813955994739e
Cai <13110818005@qq.com>
2026-01-15 12:41:39 +0800

    feat: 添加微信-小程序-掌上公交规则

--- src/apps/com.tencent.mm.ts
+++ src/apps/com.tencent.mm.ts
@@ -1,7 +1,54 @@
 import { defineGkdApp } from '@gkd-kit/define';
 
-export default defineGkdApp({
+export default defineGkdApp(
+{
   id: 'com.tencent.mm',
   name: '微信',
-  groups: [],
+  groups: [
+    {
+      key: 0,
+      name: '小程序-掌上公交-卡片广告',
+      desc: '点击[广告]-点击[不感兴趣]-点击[与我无关]',
+      activityIds: [
+        '.plugin.appbrand.ui.AppBrandUI00',
+      ],
+      rules: [
+        {
+          key: 0,
+          fastQuery: false,
+          matches: [
+            'TextView + TextView[text="广告"] + Image[text="feedback_icon"] <n View',
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/i/24632424',
+          ],
+        },
+        {
+          //preKeys: 0,
+          key: 1,
+          name: '点击[不感兴趣]',
+          fastQuery: false,
+          anyMatches: [
+            'TextView + TextView[text="不感兴趣"] + TextView[text="反馈问题"] <n View > TextView[text="不感兴趣"]'
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/i/24632404',
+          ],
+        },
+        {
+          //preKeys: 1,
+          key: 2,
+          name: '点击[与我无关]',
+          fastQuery: false,
+          anyMatches: [
+            'TextView[text="与我无关"] + TextView[text="重复收到多次"] + TextView[text="内容太差"] <n View > TextView[text="与我无关"]'
+          ],
+          snapshotUrls: [
+            'https://i.gkd.li/i/24632423',
+          ],
+        },
+      ],
+    },
+
+  ],
 });
--- src/subscription.ts
+++ src/subscription.ts
@@ -4,12 +4,12 @@ import categories from './categories';
 import globalGroups from './globalGroups';
 
 export default defineGkdSubscription({
-  id: 233,
-  name: 'Subscription',
-  version: 0,
-  author: 'author',
-  checkUpdateUrl: './gkd.version.json5',
-  supportUri: 'https://github.com/gkd-kit/subscription-template',
+  id: 114516,
+  name: 'Cai的AIsouler规则扩展',
+  version: 1,
+  author: 'Cai',
+  checkUpdateUrl: 'https://raw.githubusercontent.com/ACaiCat/aIsouler-subscription-extention/refs/heads/main/dist/gkd.version.json5',
+  supportUri: 'https://github.com/ACaiCat/aIsouler-subscription-extention',
   categories,
   globalGroups,
   apps: await batchImportApps(`${import.meta.dirname}/apps`),
